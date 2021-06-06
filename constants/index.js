f3534491b99a870746419aece87ace81fac544d1
Büyük Hakan <kaanhakan2005@gmail.com>
2021-06-06 21:07:22 +0300

    assets added and constants created

---
+++ constants/index.js
@@ -0,0 +1,12 @@
+import icons from "./icons"
+import images from "./images"
+import theme, { FONTS, SIZES, COLORS} from "./theme"
+
+export {
+    icons,
+    theme,
+    images,
+    FONTS,
+    SIZES,
+    COLORS   
+}
